be3605b2a222e45acaf33c9ae872252f2b35737a
SaqibBhatt <bhattisaqib616@gmail.com>
2024-10-30 23:29:12 +0500

    somechange

--- src/Api/api.tsx
+++ src/Api/api.tsx
@@ -1,21 +1,21 @@
 import axios from "axios"
 
-export const getMethod=(api)=>{
+export const getMethod=(api: string)=>{
     return axios.get(api)
 };
 
-export const getMethodbyId=(api,id)=>{
+export const getMethodbyId=(api: any,id: any)=>{
     return axios.get(`${api}/${id}`)
 };
 
-export const postMethod=(api,data)=>{
+export const postMethod=(api: string,data: any)=>{
     return axios.post(api,data)
 };
 
-export const updateMethod=(api,id,data)=>{
+export const updateMethod=(api: any,id: any,data: any)=>{
     return axios.put(`${api}/${id}`,data)
 };
 
-export const deleteMethod=(api,id)=>{
+export const deleteMethod=(api: any,id: any)=>{
     return axios.delete(`${api}/${id}`)
 };
